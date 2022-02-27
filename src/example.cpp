290c97b93b32b25bbb076656c1aa51c5d251aa39
Trevor Schupbach <trevorschupbach@gmail.com>
2022-02-26 23:57:02 -0500

    no more tight loop in the example

--- src/example.cpp
+++ src/example.cpp
@@ -16,33 +16,10 @@ int main()
 	int number = 5;
 	scisl::registerVar("num", number);
 
-	//scisl::program* prog = scisl::compile("src/example.scisl");
-
-	std::string line = "apple,orange,c++,scisl,1337,Flood";
-	scisl::registerVar("line", line);
-
-	std::string first = "";
-	std::string second = "";
-	std::string third = "";
-	std::string fourth = "";
-	std::string fifth = "";
-	std::string last = "";
-	scisl::registerVar("first", first);
-	scisl::registerVar("second", second);
-	scisl::registerVar("third", third);
-	scisl::registerVar("fourth", fourth);
-	scisl::registerVar("fifth", fifth);
-	scisl::registerVar("last", last);
-	scisl::program* prog = scisl::compile("Tests/function/ParseStrFuncs.scisl");
-
-	for (unsigned int i = 0; i < 1000000000; i++)
-	{
-		
-		prog->run();
-	}
+	scisl::program* prog = scisl::compile("src/example.scisl");
 
 	if (prog == nullptr) return -1;
-	//prog->decompile("src/DECOMP_example.scisl");
+	prog->decompile("src/DECOMP_example.scisl");
 	
 	number = prog->run();
 
